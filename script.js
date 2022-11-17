46441a3e5c69c1ea2492eaec3f01412aed1638c6
Brett Bates <brettbates86@gmail.com>
2022-11-17 14:06:53 -0600

    add to js

--- script.js
+++ script.js
@@ -23,8 +23,89 @@ function startQuiz() {
     totalTime = 30;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
+
+    startDiv.style.display = 'none';
+    questionDiv.style.display = 'block';
+    timer.style.display = 'block';
+    timesUp.style.display = 'none';
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length -1) {
+                gameOver();
+            }
+        }
+    },1000);
+
+    showQuiz();
+};
+
+function startGame() {
+    nextQuestion();
 }
 
+function nextQuestion() {
+    questionTitle.textContent = questions[questionIndex].question;
+    choiceA.textContent = questions[questionIndex].choices[0];
+    choiceB.textContent = questions[questionIndex].choices[1];
+    choiceC.textContent = questions[questionIndex].choices[2];
+    choiceD.textContent = questions[questionIndex].choices[3];
+
+    function checkAnswer(answer) {
+
+        var lineBreak = document.getElementById("lineBreak");
+        lineBreak.style.display = "block";
+        answerCheck.style.display = "block";
+          
+        if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+            // correct answer, add 1 score to final score
+            correctAns++;
+            // console.log(correctAns);
+            answerCheck.textContent = "Correct!";
+        } else {
+            // wrong answer, deduct 10 second from timer
+            totalTime -= 10;
+            timeLeft.textContent = totalTime;
+            answerCheck.textContent = "Wrong! The correct answer is: " + questions[questionIndex].answer;
+        }
+    
+        questionIndex++;
+        // repeat with the rest of questions 
+        if (questionIndex < questions.length) {
+            nextQuestion();
+        } else {
+            // if no more question, run game over function
+            gameOver();
+        }
+    }
+
+    function chooseA() { checkAnswer(0); }
+
+    function chooseB() { checkAnswer(1); }
+
+    function chooseC() { checkAnswer(2); }
+
+    function chooseD() { checkAnswer(3); }
+
+    //when questions are answered or timer ends, GAMEOVER!
+
+    function gameOver() {
+        summary.style.display = 'block';
+        questionDiv.style.display = 'none';
+        startDiv.style.display = 'none';
+        timer.style.display = 'none';
+        timesUp.style.display = 'block';
+
+        //final score appears
+        finalScore.textContent = correctAns;
+    }
+
+    function storeHighScores(event) {
+        event.preventDefault();
+    }
 var questions = [
     {
         number: 1,
